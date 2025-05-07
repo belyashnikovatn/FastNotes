14f506e8b3011fa01da8ddbe9bf1ee8fdee609bd
belyashnikovatn <101242667+belyashnikovatn@users.noreply.github.com>
2025-05-07 15:22:40 +0500

    Refactor database setup and connection management in FastAPI application

--- src/app/db.py
+++ src/app/db.py
@@ -1,12 +1,30 @@
 import os
 
-from database import Database
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import (
+    create_engine,
+    MetaData,
+    Column,
+    DateTime,
+    Integer,
+    String,
+    Table,
+)
+from sqlalchemy.sql import func
+from databases import Database
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
+
+notes = Table(
+    "notes",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(50), nullable=False),
+    Column("description", String(50), nullable=False),
+    Column("created_at", DateTime, server_default=func.now()),
+)
 
 database = Database(DATABASE_URL)
